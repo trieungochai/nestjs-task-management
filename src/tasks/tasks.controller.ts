d978144f8574bd86f91e29c4db77e286860b93b9
Macbook <trieungochai.dev@gmail.com>
2020-11-02 13:33:46 +0700

    Defining a Task Model

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,13 @@
 import { TasksService } from './tasks.service';
 import { Controller, Get } from '@nestjs/common';
+import { Task } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getAllTasks() {
+  getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
   }
 }
