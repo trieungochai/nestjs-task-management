a8628ad0c8e6f125bdcdac32d994b6d006895617
Macbook <trieungochai.dev@gmail.com>
2020-11-15 15:14:42 +0700

    Applying Configuration - Codebase (Part 1)

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,11 @@ import { Logger } from '@nestjs/common';
 import * as config from 'config';
 
 async function bootstrap() {
+  const serverConfig = config.get('server');
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
 
-  const serverConfig = config.get('server');
-  console.log(serverConfig);
-
-  const port = 3000;
+  const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
 }
