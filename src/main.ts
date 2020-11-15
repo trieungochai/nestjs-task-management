21502c016257107b624791ab168016ecdc6d45ec
Macbook <trieungochai.dev@gmail.com>
2020-11-15 14:44:22 +0700

    Configuration Management Set-up

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import * as config from 'config';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
+
+  const serverConfig = config.get('server');
+  console.log(serverConfig);
 
   const port = 3000;
   await app.listen(port);
